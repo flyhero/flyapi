fa4cce9c967086c7c386c04e119203ed50ecbf40
flyhero <qfwang666@163.com>
2017-11-21 11:35:15 +0800

    更改log输出位置

--- flyapi-web/src/main/java/com/flyapi/web/listener/StartListener.java
+++ flyapi-web/src/main/java/com/flyapi/web/listener/StartListener.java
@@ -16,7 +16,7 @@ public class StartListener implements ApplicationListener<ContextRefreshedEvent>
 
     public void onApplicationEvent(ContextRefreshedEvent event)
     {
-        if(event.getApplicationContext().getParent() == null)//root application context 没有parent，他就是老大.
+        if(event.getApplicationContext().getParent() == null)
         {
             //需要执行的逻辑代码，当spring容器初始化完成后就会执行该方法。
             for (String s : BANNER){
