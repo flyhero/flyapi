417e0014bed29c4256987975cc20867df9489f27
flyhero <qfwang666@163.com>
2017-12-29 00:07:30 +0800

    整理js

--- flyapi-web/src/main/java/com/flyapi/web/controller/IndexController.java
+++ flyapi-web/src/main/java/com/flyapi/web/controller/IndexController.java
@@ -1,18 +1,37 @@
 package com.flyapi.web.controller;
 
 import com.flyapi.core.base.BaseController;
+import com.flyapi.core.constant.JSONResult;
+import com.flyapi.model.SettingCarousel;
+import com.flyapi.service.api.SettingCarouselService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Author: qfwang
  * Date: 2017-12-26 下午8:33
  */
+@Controller
 public class IndexController extends BaseController{
 
-    @RequestMapping("/")
+    @Autowired
+    private SettingCarouselService settingCarouselService;
+
+    @RequestMapping("index.html")
     public ModelAndView index(){
+
+        List<SettingCarousel> list = null;
+        try {
+            list = settingCarouselService.findList();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
         mv.setViewName("index");
+        mv.addObject("carouselList",list);
         return mv;
     }
 }
