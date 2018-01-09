581c8a17e52f74a79bc86aea6675f7856a237335
flyhero <qfwang666@163.com>
2018-01-10 00:32:14 +0800

    修改消息界面

---
+++ flyapi-service/src/main/java/com/flyapi/service/impl/OpenSourceServiceImpl.java
@@ -0,0 +1,24 @@
+package com.flyapi.service.impl;
+
+import com.flyapi.core.base.BaseServiceImpl;
+import com.flyapi.dao.OpenSourceMapper;
+import com.flyapi.dao.SettingStoreMapper;
+import com.flyapi.dao.UcenterUserMapper;
+import com.flyapi.model.OpenSource;
+import com.flyapi.model.SettingStore;
+import com.flyapi.model.UcenterUser;
+import com.flyapi.service.api.OpenSourceService;
+import com.flyapi.service.api.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * author: flyhero
+ * Date: 2018/1/10 上午12:22
+ */
+@Service
+@Transactional
+public class OpenSourceServiceImpl extends BaseServiceImpl<OpenSource,OpenSourceMapper> implements OpenSourceService {
+
+}
