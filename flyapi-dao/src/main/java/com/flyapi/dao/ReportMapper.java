26f3b96712e052ef83a2775f59a119a5c0bb3836
flyhero <qfwang666@163.com>
2018-01-11 21:45:50 +0800

    feat:增加举报相关
    增加举报表和数据操作层的方法

---
+++ flyapi-dao/src/main/java/com/flyapi/dao/ReportMapper.java
@@ -0,0 +1,17 @@
+package com.flyapi.dao;
+
+import com.flyapi.model.Report;
+
+public interface ReportMapper {
+    int deleteByPrimaryKey(Long reportId);
+
+    int insert(Report record);
+
+    int insertSelective(Report record);
+
+    Report selectByPrimaryKey(Long reportId);
+
+    int updateByPrimaryKeySelective(Report record);
+
+    int updateByPrimaryKey(Report record);
+}
