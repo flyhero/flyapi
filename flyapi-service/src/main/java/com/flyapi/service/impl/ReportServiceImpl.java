dbfe9a8f4a1fab9e2fb8a629959d3fbfc02e80af
flyhero <qfwang666@163.com>
2018-01-25 00:32:49 +0800

    feat:增加举报实现
    文章详情增加举报功能

---
+++ flyapi-service/src/main/java/com/flyapi/service/impl/ReportServiceImpl.java
@@ -0,0 +1,23 @@
+package com.flyapi.service.impl;
+
+import com.flyapi.core.base.BaseServiceImpl;
+import com.flyapi.dao.CmsReplyMapper;
+import com.flyapi.dao.ReportMapper;
+import com.flyapi.model.CmsReply;
+import com.flyapi.model.Report;
+import com.flyapi.service.api.ReplyService;
+import com.flyapi.service.api.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * author: flyhero
+ * Date: 2018/1/25 上午12:29
+ */
+@Service
+@Transactional
+public class ReportServiceImpl extends BaseServiceImpl<Report,ReportMapper> implements ReportService {
+    @Autowired
+    private ReportMapper reportMapper;
+}
---
+++ flyapi-web/src/main/java/com/flyapi/web/controller/ReportController.java
@@ -0,0 +1,17 @@
+package com.flyapi.web.controller;
+
+import com.flyapi.core.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * User: qfwang
+ * Date: 2018/1/25 上午12:30
+ */
+@Controller
+@RequestMapping("report")
+public class ReportController extends BaseController{
+
+
+}
