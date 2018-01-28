d31383f62b6813f23ff27e4e55d1fe9f01783ee9
flyhero <qfwang666@163.com>
2018-01-28 21:32:51 +0800

    feat:增加举报入库
    举报功能进一步完善

--- flyapi-web/src/main/java/com/flyapi/web/controller/ReportController.java
+++ flyapi-web/src/main/java/com/flyapi/web/controller/ReportController.java
@@ -1,9 +1,19 @@
 package com.flyapi.web.controller;
 
 import com.flyapi.core.base.BaseController;
+import com.flyapi.core.constant.JSONResult;
+import com.flyapi.core.id.SnowflakeIdWorker;
+import com.flyapi.core.util.IPUtil;
+import com.flyapi.model.Report;
+import com.flyapi.service.api.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
 
 /**
  * User: qfwang
@@ -13,5 +23,34 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("report")
 public class ReportController extends BaseController{
 
+    @Autowired
+    private ReportService reportService;
+    @Autowired
+    private SnowflakeIdWorker snowflakeIdWorker;
 
+    /**
+     * 举报文章
+     * @title: report
+     * @author flyhero <http://www.iflyapi.cn>
+     * @param report
+     * @return com.flyapi.core.constant.JSONResult
+     * @date 2018/1/28 下午7:44
+     */
+    @PostMapping("article")
+    @ResponseBody
+    public JSONResult report(Report report){
+
+        //待增加：今天相同ip举报次数有限制
+
+        String ip = IPUtil.getIP(request);
+        report.setReportId(snowflakeIdWorker.nextId());
+        report.setReporterIp(ip);
+        report.setCreateTime(new Date());
+        int num = reportService.insertSelective(report);
+        if(num < 1){
+            return JSONResult.error();
+        }
+
+        return JSONResult.ok();
+    }
 }
