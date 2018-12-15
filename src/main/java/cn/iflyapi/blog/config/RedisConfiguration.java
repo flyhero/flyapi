3d1795d15b5b4a36a78fed7c8d1b2a10b70b36cd
flyhero <qfwang666@163.com>
2018-12-15 17:38:53 +0800

    feat:加入redis操作

---
+++ src/main/java/cn/iflyapi/blog/config/RedisConfiguration.java
@@ -0,0 +1,39 @@
+package cn.iflyapi.blog.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.net.UnknownHostException;
+
+
+/**
+ * author flyhero
+ * date 2018/12/15 5:32 PM
+ */
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(name = "redisTemplate")
+    public RedisTemplate<Object, Object> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory)
+            throws UnknownHostException {
+        RedisTemplate<Object, Object> template = new RedisTemplate<Object, Object>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(StringRedisTemplate.class)
+    public StringRedisTemplate stringRedisTemplate(
+            RedisConnectionFactory redisConnectionFactory)
+            throws UnknownHostException {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
--- src/test/java/cn/iflyapi/blog/FlyapiApplicationTests.java
+++ src/test/java/cn/iflyapi/blog/FlyapiApplicationTests.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,9 +17,19 @@ public class FlyapiApplicationTests {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
     @Test
     public void contextLoads() {
         System.out.println(userDao.existsUserByUsernameAndIsDelete("flyhero", true));
+    }
+
+    @Test
+    public void testRedis() {
+        redisTemplate.opsForValue().set("name", "qfwang", 100, TimeUnit.SECONDS);
+        String val = redisTemplate.opsForValue().get("name");
+        System.out.println(val);
     }
 
 }
