68dfa2fb2962aad1ec435bf3bc563f5863e5de99
flyhero <qfwang666@163.com>
2018-12-22 16:05:14 +0800

    feat:新增自定义用户表操作Mapper

---
+++ src/main/java/cn/iflyapi/blog/dao/custom/UserCustomMapper.java
@@ -0,0 +1,8 @@
+package cn.iflyapi.blog.dao.custom;
+
+import cn.iflyapi.blog.pojo.po.UserFame;
+
+public interface UserCustomMapper {
+
+    int countUserFameVal(UserFame userFame);
+}
---
+++ src/main/java/cn/iflyapi/blog/pojo/po/UserFame.java
@@ -0,0 +1,13 @@
+package cn.iflyapi.blog.pojo.po;
+
+import lombok.Data;
+
+/**
+ * @author flyhero
+ * @date 2018-12-22 3:55 PM
+ */
+@Data
+public class UserFame {
+    private Long userId;
+    private int score;
+}
